271ffe0b79b1e540fefb16db31592bfa5a1941b5
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-02 19:22:58 -0500

    changes made

--- app/src/main/java/com/example/c0765767_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0765767_f2019_mad3125_midterm/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-                
+
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         dob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
+              
                 new DatePickerDialog(MainActivity.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
